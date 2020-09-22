f66016a565df12fd77203699c47a3128896e7fe2
aman sharma1 <am00ansh@gmail.com>
2020-09-22 14:10:19 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ function App() {
       </Card>
     </div>
     <div className='bottomText'>
-    <h4>Hey I am Aman! Our world is badly effected by this virus. I pray to God that all of you will stay safe and healthy. I made this to keep track of this deadly virus handy</h4>
+    <h4>Hey I am Aman! Our world is badly affected by this virus. I pray to God that all of you stay safe and healthy. I made this to keep track of this deadly virus handy.</h4>
     </div>
    </div>
   );
